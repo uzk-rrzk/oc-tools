02967d52f0a17a393660fdf051e8b1afe1203889
Ruben Perez Vazquez <ruben.perez@uni-koeln.de>
2017-08-25 17:04:38 +0200

    (mh_edit_published_urls) Add syntax checks and documentation

--- mh_edit_published_urls.py
+++ mh_edit_published_urls.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import argparse
+import getpass
 import sys
 import pycurl
 from StringIO import StringIO
@@ -10,32 +12,20 @@ from lxml import etree
 from os import path
 
 
-# New URL to which the old ones will be converted to
-# IT HAS TO BE THE FULL URL, AS SPECIFIED IN THE MATTERHORN CONFIG
-# Example: http://pre-engage.rrz.uni-koeln.de:8080/static
-NEW_DLS =
-
 # The URLs with these protocols will not be modified
 EXCLUDE_PROTO=[ 'rtmp', 'rtmps', 'rtmpt', 'rtsp' ]
 
-# URL of the machine running the Search service where the URLs will be updated
-SEARCH_URL=
-# Digest user to access the Search service above
-USER =
-# Digest password to access the Search service above
-PASSWD =
-
-# Address of the search (get) endpoint
-SEARCH_ENDPOINT='/search/episode.xml'
+# Address of the default search (get) endpoint
+DEFAULT_SEARCH_ENDPOINT='/search/episode.xml'
 
 # Address of the search (post) endpoint
-ADD_ENDPOINT='search/add'
+DEFAULT_ADD_ENDPOINT='search/add'
 
 # Namespace to use at the mediapackages
 MP_NAMESPACE="http://mediapackage.opencastproject.org"
 
 
-def curl(mp_file, server, endpoint):
+def curl(mp_file, server, endpoint, user, password):
     postparams = [ (u'mediapackage', quote_plus(mp_file)) ]
 
     c = pycurl.Curl()
@@ -46,7 +36,7 @@ def curl(mp_file, server, endpoint):
     c.setopt(pycurl.CONNECTTIMEOUT, 2)
     c.setopt(pycurl.NOSIGNAL, 1)
     c.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
-    c.setopt(pycurl.USERPWD, USER + ':' + PASSWD)
+    c.setopt(pycurl.USERPWD,  user + ':' + password)
     c.setopt(pycurl.HTTPHEADER, ['X-Requested-Auth: Digest', 'X-Opencast-Matterhorn-Authorization: true'])
     c.setopt(pycurl.HTTPPOST, postparams)
     c.setopt(pycurl.WRITEFUNCTION, b.write)
@@ -68,16 +58,21 @@ def main(argv=None):
     b = StringIO()
 
     # Filters out the server address
-    dls = urlparse(NEW_DLS)
+    dls = urlparse(argv.new_dls)
+
+    if not argv.user:
+        setattr(argv, "user", raw_input("Enter the digest authentication user: "))
+    if not argv.password:
+        setattr(argv, "password", getpass.getpass("Enter the digest authentication password: "))
 
     try:
         # Read the elements published in the search index
-        c.setopt(pycurl.URL, urljoin(SEARCH_URL, SEARCH_ENDPOINT))
+        c.setopt(pycurl.URL, urljoin(argv.search_url, argv.search_endpoint))
         c.setopt(pycurl.FOLLOWLOCATION, False)
         c.setopt(pycurl.CONNECTTIMEOUT, 2)
         c.setopt(pycurl.NOSIGNAL, 1)
         c.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
-        c.setopt(pycurl.USERPWD, USER + ':' + PASSWD)
+        c.setopt(pycurl.USERPWD, argv.user + ':' + argv.password)
         c.setopt(pycurl.HTTPHEADER, ['X-Requested-Auth: Digest', 'X-Opencast-Matterhorn-Authorization: true'])
         c.setopt(pycurl.WRITEFUNCTION, b.write)
         #c.setopt(pycurl.VERBOSE, True)
@@ -89,7 +84,7 @@ def main(argv=None):
             document = etree.fromstring(b.getvalue())
 
             # Modify curl settings to perform an ADD in the index instead
-            c.setopt(pycurl.URL, urljoin(SEARCH_URL, ADD_ENDPOINT))
+            c.setopt(pycurl.URL, urljoin(argv.search_url, argv.add_endpoint))
             c.setopt(pycurl.POST, 1)
 
             # For every mediapackage in the results...
@@ -102,7 +97,7 @@ def main(argv=None):
                     aux = list(parsed)
 
                     # Ignore if the protocol is in the list of excluded one
-                    if parsed.scheme in EXCLUDE_PROTO:
+                    if parsed.scheme in argv.excluded_protocols:
                         print "Excluding URL {}\n".format(url.text)
                         continue
 
@@ -136,7 +131,7 @@ def main(argv=None):
                 # Overwrite the mediapackage in the index
                 if modified:
                     # Upload the mediapackage back to the search index (overwriting the old version)
-                    curl(etree.tostring(mp, encoding="UTF-8"), SEARCH_URL, ADD_ENDPOINT)
+                    curl(etree.tostring(mp, encoding="UTF-8"), argv.search_url, argv.add_endpoint, argv.user, argv.password)
                 else:
                     pass
                     
@@ -152,5 +147,37 @@ def main(argv=None):
         b.close()
 
 
+def lower_str(str):
+    """
+    Returns a lowercase string. This is to make the checks for the excluded protocols case insensitive
+    """
+    return str.lower()
+
+
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+
+    # Argument parser
+    parser = argparse.ArgumentParser(description="Edit URLs in mediapackages published in Opencast, for instance when a download server URL changes.")
+
+    parser.add_argument('search_url', help='URL of the machine running the Search service where the URLs will be updated')
+    parser.add_argument(
+        'new_dls',
+        help='New URL to which the old ones will be converted to. IT HAS TO BE THE FULL URL, AS SPECIFIED IN THE MATTERHORN CONFIG. '
+        'Example: http://pre-engage.rrz.uni-koeln.de:8080/static')
+    parser.add_argument(
+        'excluded_protocols', nargs='*', default=EXCLUDE_PROTO, type=lower_str,
+        help='A list of space-separated URI protocols that shall not be modified. If none given, defaults to ''{0}'''.format(EXCLUDE_PROTO))
+    parser.add_argument('-u', '--user', help='Digest user to access the Search service')
+    parser.add_argument('-p', '--password', help='Digest password to access the Search service')
+    parser.add_argument(
+        '-s', '--search_endpoint', default=DEFAULT_SEARCH_ENDPOINT,
+        help='Endpoint, relative to the server URL, that should return the list of published mediapackages. (Default: ''{0}'')'.format(DEFAULT_SEARCH_ENDPOINT))
+    parser.add_argument(
+        '-a', '--add_endpoint', default=DEFAULT_ADD_ENDPOINT,
+        help='Endpoint, relative to the server URL, that should edit a published mediapackage. (Default: ''{0}'')'.format(DEFAULT_ADD_ENDPOINT))
+
+    #print(parser.parse_args())
+    #exit(0)
+
+
+    sys.exit(main(parser.parse_args()))
